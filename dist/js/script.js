0cb8d6810042ec5310e8dd7a01d18b821d95129d
Yury <the.saburchik@gmail.com>
2021-02-15 23:41:36 +0700

    adding code

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,9 @@
-// test
+let hamburger = document.querySelector('#burger');
+let menu = document.querySelector('#menu');
+let nav = document.querySelector('#nav');
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+})
