5f61b7fe6fdd6385b7401e489181f477d7e2eaa8
Yury <the.saburchik@gmail.com>
2021-02-16 23:22:33 +0700

    adding code

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,19 @@
+let header = document.querySelector('#header');
 let hamburger = document.querySelector('#burger');
 let menu = document.querySelector('#menu');
 let nav = document.querySelector('#nav');
 
 hamburger.addEventListener('click', function () {
+    header.classList.toggle("active");
     hamburger.classList.toggle("active");
     menu.classList.toggle('active');
     nav.classList.toggle('active');
 })
+
+function lockScroll() {
+    if ($('body').hasClass('lock-scroll')) {
+        $('body').removeClass('lock-scroll');
+    } else {
+        $('body').addClass('lock-scroll');
+    }
+}
