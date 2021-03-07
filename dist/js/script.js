dc29514ec14460fef52a3728853ec065c780f6ae
Yury <the.saburchik@gmail.com>
2021-03-07 19:49:39 +0700

    adaptive is ready

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,11 @@
-
+let header = document.querySelector('#header');
 let hamburger = document.querySelector('#burger');
-let menu = document.querySelector('#menu');
+let navbar = document.querySelector('#navbar');
 let nav = document.querySelector('#nav');
 
 hamburger.addEventListener('click', function () {
+    header.classList.toggle("active");
     hamburger.classList.toggle("active");
-    menu.classList.toggle('active');
+    navbar.classList.toggle('active');
     nav.classList.toggle('active');
 })
-
-function lockScroll() {
-    if ($('body').hasClass('lock-scroll')) {
-        $('body').removeClass('lock-scroll');
-    } else {
-        $('body').addClass('lock-scroll');
-    }
-}
