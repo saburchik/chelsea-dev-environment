5f61a69afc8cef0687db0b33802c719fb1941b3d
Yury <the.saburchik@gmail.com>
2021-03-10 23:57:25 +0700

    debugging scripts

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,8 @@
-const header = document.querySelector('#header');
-const hamburger = document.querySelector('#burger');
-const navbar = document.querySelector('#navbar');
-const nav = document.querySelector('#nav');
-const ham = document.querySelector('#ham__effect');
+// const header = document.querySelector('#header');
+const hamburger = document.querySelector('#header, #burger, #navbar, #nav, #ham__effect');
+// const navbar = document.querySelector('#navbar');
+// const nav = document.querySelector('#nav');
+// const ham = document.querySelector('#ham__effect');
 
 hamburger.addEventListener('click', function () {
     header.classList.toggle("active");
@@ -16,31 +16,33 @@ window.addEventListener("scroll", checkScroll);
 
 function checkScroll() {
     let scrollHeight = window.scrollY;
-    if (scrollHeight > 500) {
+
+    if (scrollHeight > 550) {
         header.classList.add("fixed");
     } else {
         header.classList.remove("fixed");
     };
 
-    let scrollWidth = window.scrollX;
-    if (scrollWidth <= 770) {
+    let scrollWidth = window.scrollY;
+    if (scrollWidth >= 770) {
         header.classList.remove("active");
         hamburger.classList.remove("active");
         navbar.classList.remove('active');
         nav.classList.remove('active');
         ham.classList.remove('active');
-    };
+    }
 }
 
-const anchors = document.querySelectorAll('a[href*="#"]')
+// Smooth scroll
+const links = document.querySelectorAll('a[href*="#"], [href*="#"]')
 
-for (let anchor of anchors) {
-    anchor.addEventListener('click', function (e) {
+for (let link of links) {
+    link.addEventListener('click', function (e) {
         e.preventDefault()
 
-        const blockID = anchor.getAttribute('href').substr(1)
+        const scrollSmooth = link.getAttribute('href').substr(1)
 
-        document.getElementById(blockID).scrollIntoView({
+        document.getElementById(scrollSmooth).scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         })
