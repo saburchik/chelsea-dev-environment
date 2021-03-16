249ab2f2e6ac385a8cbc39f4e48c0d49e5182209
Yury <the.saburchik@gmail.com>
2021-03-16 23:24:35 +0700

    developer design for MW

--- dist/js/main.js
+++ dist/js/main.js
@@ -67,19 +67,24 @@ for (let link of links) {
 // Modal window
 
 let modalBtns = document.querySelectorAll("*[data-modal-btn]");
+let bodyScroll = document.querySelector("body");
+let headerPadding = document.querySelector("header");
 
 for (let i = 0; i < modalBtns.length; i++) {
     modalBtns[i].addEventListener('click', function () {
         let name = modalBtns[i].getAttribute('data-modal-btn');
         let modal = document.querySelector("[data-modal-window='" + name + "']");
         modal.style.display = "block";
-        document.querySelector("body").classList.toggle('active');
-        document.querySelector("header").classList.toggle('padding');
+        bodyScroll.classList.toggle('hidden');
+        headerPadding.classList.toggle('padding');
 
         let close = modal.querySelector(".closed__modal");
         close.addEventListener('click', function () {
             modal.style.display = "none";
+            bodyScroll.classList.remove('hidden');
+            headerPadding.classList.remove('padding');
         })
+
     })
 }
 
@@ -88,8 +93,8 @@ window.onclick = function (e) {
         let modals = document.querySelectorAll("*[data-modal-window]");
         for (let i = 0; i < modals.length; i++) {
             modals[i].style.display = "none";
-            document.querySelector("body").classList.remove('active');
-            document.querySelector("header").classList.remove('padding');
+            bodyScroll.classList.remove('hidden');
+            headerPadding.classList.remove('padding');
         }
     }
 }
