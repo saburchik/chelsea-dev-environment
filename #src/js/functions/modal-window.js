3a9b9753da460a1f8f879bfeaa1bcb07c4ed0100
Yury <the.saburchik@gmail.com>
2021-04-04 21:48:04 +0700

    cleaning the code

--- #src/js/functions/modal-window.js
+++ #src/js/functions/modal-window.js
@@ -1,68 +1,3 @@
-// Add classes, when a burger is clicked on
-const burger = document.querySelector('#burger');
-const header = document.querySelector('#header');
-const navbar = document.querySelector('#navbar');
-const nav = document.querySelector('#nav');
-const ham = document.querySelector('#effect');
-
-burger.addEventListener('click', function () {
-    header.classList.toggle('active');
-    navbar.classList.toggle('active');
-    nav.classList.toggle('active');
-    ham.classList.toggle('active');
-});
-// Fixed header
-window.addEventListener("scroll", checkScroll);
-function checkScroll() {
-    let scrollHeight = window.scrollY;
-
-    if (scrollHeight > 550) {
-        header.classList.add("fixed");
-    } else {
-        header.classList.remove("fixed");
-    };
-};;
-// When width display > 770px - all classes for burger - DISABLE 
-onload = function () {
-    widthMenu();
-}
-
-window.onresize = function () {
-    widthMenu();
-}
-
-function widthMenu() {
-    let width = window.innerWidth;
-
-    if (width > 770) {
-        document.getElementById('header').classList.remove("active");
-        document.getElementById('burger').classList.remove("active");
-        document.getElementById('navbar').classList.remove("active");
-        document.getElementById('nav').classList.remove("active");
-        document.getElementById('effect').classList.remove("active");
-    }
-};
-// Smooth scroll
-const links = document.querySelectorAll('a[href*="#"], [href*="#"]');
-
-for (let link of links) {
-    link.addEventListener('click', function (e) {
-        e.preventDefault()
-
-        const scrollSmooth = link.getAttribute('href').substr(1)
-
-        document.getElementById(scrollSmooth).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        })
-
-        // Disable burger menu, when smooth scroll
-        header.classList.remove('active');
-        navbar.classList.remove('active');
-        nav.classList.remove('active');
-        ham.classList.remove('active');
-    })
-};
 const modalLinks = document.querySelectorAll('.modal-link');
 const body = document.querySelector('body');
 const lockPadding = document.querySelectorAll('.lock-padding');
@@ -156,4 +91,4 @@ document.addEventListener('keydown', function (e) {
         const modalActive = document.querySelector('.modal.open');
         modalClose(modalActive);
     }
-});;
+});
