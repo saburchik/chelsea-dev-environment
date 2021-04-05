63b2bb25a2fc2ad4a8ba8b5508aa95b9e6a16949
Yury <the.saburchik@gmail.com>
2021-04-05 18:48:49 +0700

    cleaning the code

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,7 +16,7 @@ window.addEventListener("scroll", checkScroll);
 function checkScroll() {
     let scrollHeight = window.scrollY;
 
-    if (scrollHeight > 550) {
+    if (scrollHeight > 490) {
         header.classList.add("fixed");
     } else {
         header.classList.remove("fixed");
