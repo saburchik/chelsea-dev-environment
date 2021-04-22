acaf278f65f2ce8e11b563edd9a2d927b09cbf6e
Yury <the.saburchik@gmail.com>
2021-04-22 22:58:00 +0700

    update

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,15 +1,5 @@
-// import smoothscroll from 'smoothscroll-polyfill';
-
-// smoothscroll.polyfill();
-
-// window.SVGElement.prototype.scrollIntoView = (window.HTMLElement || window.Element).prototype.scrollIntoView;
-
 @@include('functions/hamburger.js');
 @@include('functions/fixed-header.js');
 @@include('functions/adaptiveWidth.js');
 @@include('functions/smooth-scroll.js');
 @@include('functions/modal-window.js');
-
-// import smoothscroll from 'smoothscroll-polyfill';
-// kick off the polyfill!
-// smoothscroll.polyfill(); 
